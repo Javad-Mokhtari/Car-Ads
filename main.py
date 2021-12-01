dd552545184ad3a2b3619d8714218e0e8689e9b8
javad-mokhtari <javad.mktri@gmail.com>
2021-12-02 02:11:29 +0330

    initial changes for test

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from DivarScraping import *
+
+
+def main():
+    # Scraping car information from https://divar.ir/
+    urls = car_ad_links(200)
+    for url in urls:
+        if car_information(url):
+            print(car_information(url))
+
+
 if __name__ == '__main__':
-    print('PyCharm')
+    main()
