5c67d7be1ad480196044596631a14a91883707a5
javad-mokhtari <javad.mktri@gmail.com>
2021-12-02 21:07:41 +0330

    latest_ads() function is created

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from DivarScraping import *
-import SaveData
-from SaveData import insert_data
+import Database
+from Database import insert_data
+import os, stat
 
 
-def main():
-    # Scraping car information from https://divar.ir/
-    urls = car_ad_links(200)
+def save_data(n):
+    urls = car_ad_links(n)
     for url in urls:
         data = car_data(url)
-        if data:
-            print(data)
+        if data and data != "Error!":
             insert_data(data)
 
 
-if __name__ == '__main__':
-    main()
+def latest_ads(n):
+    counter = 1
+    num = n * 3
+    file = open("latest-car-ads.txt", 'a')
+    urls = car_ad_links(num)
+    for url in urls:
+        if counter <= n:
+            car_info = car_information(url)
+            if car_info:
+                for key, value in car_info.items():
+                    file.write('{0:>15s}:  {1:>20s}\t'.format(key, value))
+                counter += 1
+                file.write('\n')
+    file.close()
+
+
+latest_ads(30)
