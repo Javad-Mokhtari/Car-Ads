4f8958ba059d870df6f3f454596c29949b23a5d6
javad-mokhtari <javad.mokhtari.976@gmail.com>
2021-12-03 21:06:40 +0330

    adding ID features for ads in car table

--- Database.py
+++ Database.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from DivarScraping import car_data
+import pandas as pd
 
 
 # Connect to MySQL
@@ -9,9 +10,9 @@ my_cursor = my_db.cursor()
 my_cursor.execute("CREATE DATABASE IF NOT EXISTS CarInfo;")
 my_db = mysql.connector.connect(host='127.0.0.1', user='root', password='javad76mi', database='CarInfo')
 my_cursor = my_db.cursor()
-my_cursor.execute("""CREATE TABLE IF NOT EXISTS CarFeatures(brand VARCHAR(255), model VARCHAR(255), year INT(255),
-     worked INT(255), price BIGINT(255), color VARCHAR(255), engine_status VARCHAR(255), chassis_status VARCHAR(255),
-     body_status VARCHAR(255), insurance_deadline INT(255));""")
+my_cursor.execute("""CREATE TABLE IF NOT EXISTS CarFeatures(ID VARCHAR(255), brand VARCHAR(255), model VARCHAR(255),
+                     year INT(255), worked INT(255), price BIGINT(255), color VARCHAR(255), engine_status VARCHAR(255),
+                     chassis_status VARCHAR(255), body_status VARCHAR(255), insurance_deadline INT(255));""")
 
 
 def insert_data(data_dic):
@@ -20,3 +21,12 @@ def insert_data(data_dic):
         values.append(data_dic[column])
     my_cursor.execute("INSERT INTO CarFeatures VALUES {};".format(tuple(values)))
     my_db.commit()
+
+
+def export_data():
+    dataframe = pd.read_sql("SELECT * FROM CarFeatures;", my_db)
+    dataframe.to_csv("Data/Cars-data.csv")
+
+
+def clean_duplicated():
+    pass
