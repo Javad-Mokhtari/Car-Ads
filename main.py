58cc59ed51d01dbe9e3bcb2f25474ce23b121994
javad-mokhtari <javad.mokhtari.976@gmail.com>
2021-12-03 21:08:57 +0330

    difined two function for save data to database

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 from DivarScraping import *
+from Database import *
 import Database
-from Database import insert_data
+import re
 
 
-def save_data(n):
-    urls = car_ad_links(n)
+def save_latest_ads(n):
+    number = int(n * 1.5)
+    urls = car_ad_links(number, "https://divar.ir/s/tehran/auto")
     for url in urls:
         data = car_data(url)
-        if data and data != "Error!":
+        if data != "Error!":
             insert_data(data)
 
 
-def latest_ads(n):
-    counter = 1
-    num = n * 3
-    file = open("latest-car-ads.txt", 'a')
-    urls = car_ad_links(num)
-    for url in urls:
-        if counter <= n:
-            car_info = car_information(url)
-            if car_info:
-                for key, value in car_info.items():
-                    file.write('{0:20s}: {1:25s}\n'.format(key, value))
-                counter += 1
-                file.write('\n\n\n')
-    file.close()
+def save_favorite_ads(brand, model, n):
+    ads_url = 'https://divar.ir/s/tehran/car/' + brand + '/' + model
+    if requests.get(ads_url).ok:
+        urls = car_ad_links(n, ads_link)
+        for url in urls:
+            data = car_data(url)
+            if data != "Error!":
+                insert_data(data)
+    else:
+        return "Error!"
 
 
-latest_ads(30)
+save_latest_ads(100)
