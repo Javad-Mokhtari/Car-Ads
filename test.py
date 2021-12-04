61359145ab76125e50db5b26f4326f946192916e
javad-mokhtari <javad.mktri@gmail.com>
2021-12-04 21:34:07 +0330

    Nothing! It's clear.

--- test.py
+++ test.py
@@ -3,4 +3,3 @@ from bs4 import BeautifulSoup
 import html5lib
 import re
 
-values = data_dic['brand'], data_dic['model'], data_dic['year'], data_dic['worked'], data_dic['price'], data_dic['color'], data_dic['engine_status'], data_dic['chassis_status'], data_dic['body_status'], data_dic['insurance_deadline']
