f1610f45943419335be05a7efd9d69d7de8ae955
javad-mokhtari <javad.mktri@gmail.com>
2021-12-15 22:07:40 +0330

    main function is writed

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from DivarScraping import *
 from Database import *
+from DivarScraping import *
 from DataAnalysis import *
 
 
 def main():
-    save_latest_ads(400)
+    pass
+    # database_settings()
+    # With this function we can create database and table and connect to it
+
+    # save_latest_ads(200)
+    # We used this function for save the last 200 ads
+
+    # save_desired_ads('pride', '131', 10)
+    # Also, we can save ads with desired brand and models to desired number
+
+    # export_data()
+    # for saving a csv file of data to 'Saved_Data' folder
+
+    # get_ads()
+    # get_ads() function shows the ads with desired brand and model and saves them as a .txt file to 'Saved_Data' folder
+
+    # latest_ads()
+    # latest_ads() shows the latest ads and saves them as a .txt file to 'Saved_Data' folder
+
+    # frequency()
+    # The frequency() function shows the amount of frequncy in terms of brands and color in ads. Also, displays the
+    # amount of used from car compared to year of production in a scatter chart and saves them to 'Saved_Data' folder
 
 
 if __name__ == "__main__":
