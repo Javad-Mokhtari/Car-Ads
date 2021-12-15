a42ec10fbfddb8d57d2dc36ccf8b56ec9da30d56
javad-mokhtari <javad.mktri@gmail.com>
2021-12-15 17:20:32 +0330

    Two funtion moved from main.py to database.py

--- Database.py
+++ Database.py
@@ -3,17 +3,19 @@ from DivarScraping import car_data
 import pandas as pd
 
 
-# Connecting to localhost
-my_db = mysql.connector.connect(host='127.0.0.1', user='root', password='javad76mi')
-my_cursor = my_db.cursor()
-# Creating database
-my_cursor.execute("CREATE DATABASE IF NOT EXISTS CarInfo;")
-my_db = mysql.connector.connect(host='127.0.0.1', user='root', password='javad76mi', database='CarInfo')
-my_cursor = my_db.cursor()
-# Creating table with its features
-my_cursor.execute("""CREATE TABLE IF NOT EXISTS CarFeatures(ID VARCHAR(255), brand VARCHAR(255), model VARCHAR(255),
-    year INT(255), worked INT(255), price BIGINT(255), color VARCHAR(255), engine_status VARCHAR(255),
-    chassis_status VARCHAR(255), body_status VARCHAR(255), insurance_deadline INT(255), PRIMARY KEY (ID));""")
+# With this function we can create database and table and connect to it
+def database_settings():
+    # Connecting to localhost
+    my_db = mysql.connector.connect(host='127.0.0.1', user='root', password='javad76mi')
+    my_cursor = my_db.cursor()
+    # Creating database
+    my_cursor.execute("CREATE DATABASE IF NOT EXISTS CarInfo;")
+    my_db = mysql.connector.connect(host='127.0.0.1', user='root', password='javad76mi', database='CarInfo')
+    my_cursor = my_db.cursor()
+    # Creating table with its features
+    my_cursor.execute("""CREATE TABLE IF NOT EXISTS CarFeatures(ID VARCHAR(255), brand VARCHAR(255), model VARCHAR(255),
+        year INT(255), worked INT(255), price BIGINT(255), color VARCHAR(255), engine_status VARCHAR(255),
+        chassis_status VARCHAR(255), body_status VARCHAR(255), insurance_deadline INT(255), PRIMARY KEY (ID));""")
 
 
 # This function is defined for saving a row to database with input dicrionary
@@ -26,6 +28,29 @@ def insert_data(data_dic):
         my_cursor.execute("INSERT INTO CarFeatures VALUES {};".format(values))
         # Applying changes to database
         my_db.commit()
+
+
+def save_latest_ads(n):
+    number = int(n * 1.5)
+    urls = car_ad_links(number, "https://divar.ir/s/tehran/auto")
+    for url in urls:
+        data = car_data(url)
+        if data != "Error!":
+            insert_data(data)
+    print("Done!")
+
+
+def save_favorite_ads(brand, model, n):
+    ads_url = 'https://divar.ir/s/tehran/car/' + brand + '/' + model
+    if requests.get(ads_url).ok:
+        urls = car_ad_links(n, ads_link)
+        for url in urls:
+            data = car_data(url)
+            if data != "Error!":
+                insert_data(data)
+        print("Done!")
+    else:
+        return "Error!"
 
 
 # It gives to us a csv file of data
--- main.py
+++ main.py
@@ -1,27 +1,11 @@
 from DivarScraping import *
 from Database import *
-import Database
-import re
+from DataAnalysis import *
 
 
-def save_latest_ads(n):
-    number = int(n * 1.5)
-    urls = car_ad_links(number, "https://divar.ir/s/tehran/auto")
-    for url in urls:
-        data = car_data(url)
-        if data != "Error!":
-            insert_data(data)
-    print("Done!")
+def main():
+    save_latest_ads(400)
 
 
-def save_favorite_ads(brand, model, n):
-    ads_url = 'https://divar.ir/s/tehran/car/' + brand + '/' + model
-    if requests.get(ads_url).ok:
-        urls = car_ad_links(n, ads_link)
-        for url in urls:
-            data = car_data(url)
-            if data != "Error!":
-                insert_data(data)
-        print("Done!")
-    else:
-        return "Error!"
+if __name__ == "__main__":
+    main()
