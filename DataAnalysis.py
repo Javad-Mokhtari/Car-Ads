aa8038d1e1e86e5b8c81adacfa90a8e2982cd8b0
javad-mokhtari <javad.mktri@gmail.com>
2021-12-15 17:19:33 +0330

    The frequency function is completed

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,10 +1,12 @@
 import mysql.connector
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 
 # The frequency() function shows to us the amount of frequncy in terms of brands and color in ads
 def frequency():
+    # Connecting to database and reading cars brand data
     my_db = mysql.connector.connect(host='127.0.0.1', user='root', password='javad76mi', database='CarInfo')
     ads_number = len(pd.read_sql("SELECT ID FROM CarFeatures;", my_db))
     brand_df = pd.read_sql("SELECT brand, COUNT(brand) FROM CarFeatures GROUP BY brand;", my_db)
@@ -20,7 +22,16 @@ def frequency():
     new_row = pd.Series(['other', sum_other], index=brand_frequency_df.columns)
     brand_frequency_df = brand_frequency_df.append(new_row, ignore_index=True)
     brand_frequency_df = brand_frequency_df.set_index('Brand')
-    plot = brand_frequency_df.plot.pie(y='frequency')
+
+    # Show frequency brands with pyplot in pie chart
+    brand_slices = brand_frequency_df['frequency'].tolist()
+    brand_labels = brand_frequency_df.index.tolist()
+    brand_fig = plt.figure(figsize=[10, 10])
+    brand_ax = brand_fig.add_subplot(111)
+    brand_ax.set_title('Cars Brand Frequency')
+    brand_ax.pie(brand_slices, labels=brand_labels, labeldistance=1.05)
+
+    # Reading cars color data to dataframe
     color_df = pd.read_sql("SELECT color, COUNT(color) FROM CarFeatures GROUP BY color;", my_db)
     color_frequency_df = pd.DataFrame(columns=['Color', 'frequency'])
     sum_other = 0
@@ -33,14 +44,27 @@ def frequency():
             color_frequency_df = color_frequency_df.append(new_row, ignore_index=True)
     new_row = pd.Series(['other', sum_other], index=color_frequency_df.columns)
     color_frequency_df = color_frequency_df.append(new_row, ignore_index=True)
-    translate_dict = {'آبی': 'blue', 'خاکستری': 'gray', 'سفید': 'white', 'سفید صدفی': 'pearl white', 'قرمز': 'red',
-                      'مشکی': 'black', 'نقرهu200cای': 'silver', 'نوکu200cمدادی': 'graphite grey', 'other': 'other'}
+    translation_dict = {'آبی': 'blue', 'خاکستری': 'gray', 'سفید': 'white', 'سفید صدفی': 'pearl white', 'قرمز': 'red',
+                        'مشکی': 'black', 'نقرهu200cای': 'silver', 'نوکu200cمدادی': 'graphite grey', 'other': 'other'}
     for i in range(len(color_frequency_df)):
         color_frequency_df = color_frequency_df.replace(color_frequency_df.at[i, 'Color'],
-                                                        translate_dict[color_frequency_df.at[i, 'Color']])
+                                                        translation_dict[color_frequency_df.at[i, 'Color']])
     color_frequency_df = color_frequency_df.set_index('Color')
-    print(color_frequency_df)
-    plot = color_frequency_df.plot.pie(y='frequency')
+
+    # Display color frequency in pie chart
+    color_slices = color_frequency_df['frequency'].tolist()
+    colors = ['blue', 'gray', 'white', '#FAF8FA', 'red', 'black', '#D3D3D3', '#A9A9A9', 'green']
+    color_labels = color_frequency_df.index.tolist()
+    color_dict = {}
+    for [l, c] in zip(color_labels, colors):
+        color_dict[l] = c
+    color_fig = plt.figure(figsize=[10, 10])
+    color_ax = color_fig.add_subplot(111)
+    pie_wedge_collection = color_ax.pie(color_slices, labels=color_labels, labeldistance=1.05)
+    for pie_wedge in pie_wedge_collection[0]:
+        pie_wedge.set_edgecolor('#99004C')
+        pie_wedge.set_facecolor(color_dict[pie_wedge.get_label()])
+    color_ax.set_title('Cars Color Frequency')
     plt.show()
 
 
